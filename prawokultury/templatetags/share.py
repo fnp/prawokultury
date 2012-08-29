09f046d960613fb4022cd7f4ad5a66a9787bc32a
Radek Czajka <radoslaw.czajka@nowoczesnapolska.org.pl>
2012-08-29 17:12:45 +0200

    social

---
+++ prawokultury/templatetags/share.py
@@ -0,0 +1,13 @@
+from django.template import Library
+
+register = Library()
+
+
+@register.inclusion_tag('share.html', takes_context=True)
+def share(context, url, description, iconset=""):
+    return {
+        'url': url,
+        'description': description,
+        'iconset': iconset,
+        'request': context['request'],
+    }
