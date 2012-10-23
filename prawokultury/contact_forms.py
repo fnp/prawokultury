2fddcc58b2cba6f92621f7c69c73ff4b96da8953
Radek Czajka <radoslaw.czajka@nowoczesnapolska.org.pl>
2012-10-23 12:07:26 +0200

    Some changes

--- prawokultury/contact_forms.py
+++ prawokultury/contact_forms.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from django import forms
 from contact.forms import ContactForm
 from django.utils.translation import ugettext_lazy as _
@@ -12,8 +13,16 @@ class RegistrationForm(ContactForm):
     organization = forms.CharField(label=_('Organization'), 
             max_length=256, required=False)
     title = forms.CharField(label=_('Title of presentation'), 
-            max_length=256, required=False)
+            max_length=256)
     presentation = forms.FileField(label=_('Presentation'),
             required=False)
-    summary = forms.CharField(label=_('Summary'),
-            widget=forms.Textarea, required=False)
+    summary = forms.CharField(label=_('Summary of presentation (max. 1800 characters)'),
+            widget=forms.Textarea, max_length=1800)
+    agree_data = forms.BooleanField(
+        label=_('Permission for data processing'),
+        help_text=_(u'I hereby grant Modern Poland Foundation (Fundacja Nowoczesna Polska, ul. Marszałkowska 84/92, 00-514 Warszawa) permission to process my personal data (name, e-mail address) for purposes of registration for CopyCamp conference.')
+    )
+    agree_license = forms.BooleanField(
+        label=_('Permission for publication'),
+        help_text=_('I agree to having materials recorded during the conference released under the terms of <a href="http://creativecommons.org/licenses/by-sa/3.0/deed">CC BY-SA</a> license.')
+    )
