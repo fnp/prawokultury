468530f4f817fae652bbb56fa2cb70e4d6b95ae5
Jan Szejko <janek37@gmail.com>
2016-09-22 13:37:53 +0200

    update top menu

--- prawokultury/menu_items.py
+++ prawokultury/menu_items.py
@@ -1,10 +1,9 @@
+# -*- coding: utf-8 -*-
 from migdal.models import Entry
-from menu.helpers import ObjectMenuItem, MenuItem
-from django.core.urlresolvers import reverse_lazy
-from django.utils.translation import ugettext_lazy as _
-
+from menu.helpers import ObjectMenuItem
 
 ITEMS = []
+
 
 def add_entry(**qs):
     try:
@@ -18,15 +17,13 @@ def add_entry(**qs):
 add_entry(slug_pl='co')
 add_entry(slug_pl='gdzie')
 add_entry(pk=25)
-#add_entry(slug_pl='program')
-#add_entry(slug_pl='faq')
+add_entry(slug_pl='program')
+# add_entry(slug_pl='faq')
 add_entry(slug_pl='wyszehrad')
-#add_entry(slug_pl='wez-udzial')
+# add_entry(slug_pl='wez-udzial')
 add_entry(slug_pl='materialy')
-#add_entry(pk=25)
-#add_entry(slug_pl='warsztaty')
+# add_entry(pk=25)
+# add_entry(slug_pl='warsztaty')
 add_entry(slug_pl='poprzednie')
 add_entry(slug_pl='kontakt')
 add_entry(slug_pl='faq')
-
-
