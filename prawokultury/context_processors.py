ac2bf5fce0bddb3d687562c70dc7f4b90758b95d
Jan Szejko <janek37@gmail.com>
2018-04-05 10:37:51 +0200

    restore "remind me" button

--- prawokultury/context_processors.py
+++ prawokultury/context_processors.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from django.core.urlresolvers import reverse
+from django.utils.translation import ugettext as _
 
 from prawokultury.contact_forms import RegistrationForm
 from .contact_forms import RegisterSpeaker
@@ -10,8 +11,11 @@ def registration_url(request):
     registration_form = RegistrationForm()
     if speaker_form.started and not speaker_form.closed:
         url = reverse("contact_form", args=["register-speaker"])
+        label = _('Registration')
     elif speaker_form.closed and not registration_form.started:
         url = reverse("contact_form", args=["remind-me"])
+        label = _('Remind me')
     else:
         url = reverse("contact_form", args=["register"])
-    return {'REGISTRATION_URL': url}
+        label = _('Registration')
+    return {'REGISTRATION_URL': url, 'REGISTRATION_BUTTON_LABEL': label}
